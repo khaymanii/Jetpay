b468e329918c6acd22e6305b70b98912de79de1a
khaymanii <kennethakpo61@gmail.com>
2024-08-21 19:31:07 +0100

    created last signup page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
-import Signup2 from "./(screens)/Signup2";
+import Signup3 from "./(screens)/Signup3";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Signup2 />{" "}
+      <Signup3 />{" "}
     </View>
   );
 }
