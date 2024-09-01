b8fe0a5de660028f4a66c191a99c65d332215cea
khaymanii <kennethakpo61@gmail.com>
2024-09-01 12:14:35 +0100

    created hero component

--- app/(screens)/SendMoney.tsx
+++ app/(screens)/SendMoney.tsx
@@ -35,6 +35,80 @@ export default function SendMoney() {
           />
         </View>
       </View>
+      <View style={styles.heroContainer}>
+        <View style={styles.usdContainer}>
+          <View>
+            <Text style={styles.inDollars}>In Dollars</Text>
+            <Text style={styles.dollars}>$31.00</Text>
+          </View>
+          <View style={styles.usdContainer1}>
+            {" "}
+            <Text style={styles.usd}>USD</Text>
+            <Image
+              source={require("../../assets/icons/arrowdownwhite.svg")}
+              resizeMode="cover"
+            />
+          </View>
+        </View>
+        <Text style={styles.usdText}>1 USD - NGN 1591, Fees = $0</Text>
+        <View style={styles.usdContainer}>
+          <View>
+            <Text style={styles.inDollars}>In Naira</Text>
+            <Text style={styles.dollars}>$50,000</Text>
+          </View>
+          <View style={styles.usdContainer1}>
+            {" "}
+            <Text style={styles.usd}>NGN</Text>
+            <Image
+              source={require("../../assets/icons/arrowdownwhite.svg")}
+              resizeMode="cover"
+            />
+          </View>
+        </View>
+        <View style={styles.transaction}>
+          <View>
+            <View style={styles.add}>
+              {" "}
+              <Image
+                source={require("../../assets/icons/add.svg")}
+                resizeMode="cover"
+              />
+            </View>
+            <Text style={styles.sendText}>Send</Text>
+          </View>
+          <View>
+            <View style={styles.add}>
+              {" "}
+              <Image
+                source={require("../../assets/icons/add.svg")}
+                resizeMode="cover"
+              />
+            </View>
+            <Text style={styles.sendText}>Add</Text>
+          </View>{" "}
+          <View>
+            <View style={styles.add}>
+              {" "}
+              <Image
+                source={require("../../assets/icons/wallet.svg")}
+                resizeMode="cover"
+              />
+            </View>
+            <Text style={styles.sendText}>Wallet</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.recentContainer}>
+        <Text style={styles.recent}>Recent</Text>
+        <View style={styles.seeMoreContainer}>
+          <Text style={styles.seeMore}>See more</Text>
+          <Image
+            source={require("../../assets/icons/arrowrightblue.svg")}
+            resizeMode="cover"
+            style={styles.arrowRight}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -51,7 +125,7 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 22,
-    fontWeight: "500",
+    fontWeight: 500,
   },
   notificationContainer: {
     width: 40,
@@ -90,4 +164,78 @@ const styles = StyleSheet.create({
   },
   acctNumber: { fontWeight: "bold" },
   copy: { width: 25, height: 25, marginTop: 3 },
+  heroContainer: {
+    width: "100%",
+    height: "auto",
+    marginVertical: 15,
+    borderRadius: 18,
+    backgroundColor: "#F6F6F7",
+    padding: 20,
+  },
+  usdContainer: {
+    width: "100%",
+    height: "auto",
+    borderRadius: 18,
+    backgroundColor: "#FFFFFF",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+  },
+  inDollars: { color: "#566BFB" },
+  dollars: { fontSize: 26, color: "#566BFB", fontWeight: 600 },
+  usdContainer1: {
+    backgroundColor: "#292A65",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    width: "auto",
+    height: "auto",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    gap: 4,
+  },
+  usd: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    fontWeight: "semibold",
+    textAlign: "center",
+  },
+  usdText: { textAlign: "center", color: "#292A65", marginVertical: 10 },
+  transaction: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  add: {
+    width: "auto",
+    height: "auto",
+    padding: 15,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+  },
+  sendText: { textAlign: "center", color: "#292A65", marginTop: 3 },
+  recentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  recent: { fontSize: 22, fontWeight: 500 },
+  seeMoreContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  seeMore: {
+    color: "#4E49C7",
+    fontSize: 14,
+    marginTop: 2,
+  },
+  arrowRight: {
+    marginTop: 4,
+  },
 });
